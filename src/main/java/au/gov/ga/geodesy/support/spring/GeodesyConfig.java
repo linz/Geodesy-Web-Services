bacd08537789194e90e266e770d5f0623432bcc3
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-06-17 16:39:30 +1000

    Register IgsSiteLogMoxyMarshaller as spring bean.

--- src/main/java/au/gov/ga/geodesy/support/spring/GeodesyConfig.java
+++ src/main/java/au/gov/ga/geodesy/support/spring/GeodesyConfig.java
@@ -1,9 +1,18 @@
 package au.gov.ga.geodesy.support.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import au.gov.ga.geodesy.igssitelog.interfaces.xml.IgsSiteLogXmlMarshaller;
+import au.gov.ga.geodesy.igssitelog.support.marshalling.moxy.IgsSiteLogMoxyMarshaller;
 
 @Configuration
 @ComponentScan(basePackages = {"au.gov.ga.geodesy"})
 public class GeodesyConfig {
+
+    @Bean
+    public IgsSiteLogXmlMarshaller siteLogMarshaller() throws Exception {
+        return new IgsSiteLogMoxyMarshaller();
+    }
 }

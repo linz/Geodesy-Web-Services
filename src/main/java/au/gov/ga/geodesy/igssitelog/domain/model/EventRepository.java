c728d18a23dcd7598b24ef80bacd13bc6ab9b955
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-06-19 13:47:48 +1000

    Add method to fetch pending events.

--- src/main/java/au/gov/ga/geodesy/igssitelog/domain/model/EventRepository.java
+++ src/main/java/au/gov/ga/geodesy/igssitelog/domain/model/EventRepository.java
@@ -1,7 +1,14 @@
 package au.gov.ga.geodesy.igssitelog.domain.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import au.gov.ga.geodesy.domain.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
+
+    @Query("select e from Event e where e.timeHandled is null")
+    List<Event> getPendingEvents();
 }

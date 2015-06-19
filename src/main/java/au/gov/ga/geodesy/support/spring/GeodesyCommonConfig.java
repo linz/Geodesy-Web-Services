7c56850b9476e60c89b0fae6e3f54f98025f1bd7
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-06-19 11:34:52 +1000

    Turn on compile-time weaving (part 2).

--- src/main/java/au/gov/ga/geodesy/support/spring/GeodesyCommonConfig.java
+++ src/main/java/au/gov/ga/geodesy/support/spring/GeodesyCommonConfig.java
@@ -3,17 +3,17 @@ package au.gov.ga.geodesy.support.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import au.gov.ga.geodesy.domain.model.EventPublisher;
-import au.gov.ga.geodesy.domain.model.EventRepositoryPublisher;
 import au.gov.ga.geodesy.igssitelog.interfaces.xml.IgsSiteLogXmlMarshaller;
 import au.gov.ga.geodesy.igssitelog.support.marshalling.moxy.IgsSiteLogMoxyMarshaller;
 
 @Configuration
 @ComponentScan(basePackages = {"au.gov.ga.geodesy"})
 @EnableWebMvc
+@EnableSpringConfigured
 public class GeodesyCommonConfig extends WebMvcConfigurerAdapter {
 
     @Bean

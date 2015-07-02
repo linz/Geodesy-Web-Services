eb268e7926c35cb00269b49e25de9bf503755c26
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-07-02 14:53:20 +1000

    Implement find node by four char id endpoint.

--- src/main/java/au/gov/ga/geodesy/domain/model/NodeRepository.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/NodeRepository.java
@@ -2,6 +2,8 @@ package au.gov.ga.geodesy.domain.model;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,8 @@ import org.springframework.data.repository.query.Param;
 public interface NodeRepository extends JpaRepository<Node, Integer> {
 
     @Query("select n from Node n where n.siteId = :siteId")
-    public List<Node> fiidBySiteId(@Param("siteId") Integer siteId);
+    public Page<Node> findBySiteId(@Param("siteId") Integer siteId, Pageable pageRequest);
+
+    @Query("select n from Node n where n.siteId = :siteId")
+    public List<Node> findBySiteId(@Param("siteId") Integer siteId);
 }
---
+++ src/main/java/au/gov/ga/geodesy/interfaces/rest/NodeSearchEndpoint.java
@@ -0,0 +1,51 @@
+package au.gov.ga.geodesy.interfaces.rest;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import au.gov.ga.geodesy.domain.model.GnssCorsSite;
+import au.gov.ga.geodesy.domain.model.GnssCorsSiteRepository;
+import au.gov.ga.geodesy.domain.model.Node;
+import au.gov.ga.geodesy.domain.model.NodeRepository;
+
+@Controller
+@RequestMapping("/nodes/search")
+public class NodeSearchEndpoint {
+
+    @Autowired
+    private NodeRepository nodes;
+
+    @Autowired
+    private GnssCorsSiteRepository sites;
+
+    @Autowired
+    private PagedResourcesAssembler<Node> assembler;
+
+    @RequestMapping(value = "/findByFourCharacterId", method = RequestMethod.GET)
+    public ResponseEntity<PagedResources<Resource<Node>>> findByFourCharacterId(
+            String id, Pageable pageRequest) {
+
+        Page<Node> page = null;
+        GnssCorsSite site = sites.findByFourCharacterId(id);
+        if (site != null) {
+            page = nodes.findBySiteId(site.getId(), pageRequest);
+        } else {
+            page = new PageImpl<Node>(new ArrayList<Node>());
+        }
+        PagedResources<Resource<Node>> paged = assembler.toResource(page);
+        return new ResponseEntity<>(paged, new HttpHeaders(), HttpStatus.OK);
+    }
+}

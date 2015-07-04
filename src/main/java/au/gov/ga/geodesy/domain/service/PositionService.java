1d1061b2e2d9c5e4a18b94ade91abe03b485824e
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-07-04 16:26:20 +1000

    Extract estimated coordinates from SINEX files.

---
+++ src/main/java/au/gov/ga/geodesy/domain/service/PositionService.java
@@ -0,0 +1,101 @@
+package au.gov.ga.geodesy.domain.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import au.gov.ga.geodesy.domain.model.Event;
+import au.gov.ga.geodesy.domain.model.EventPublisher;
+import au.gov.ga.geodesy.domain.model.EventSubscriber;
+import au.gov.ga.geodesy.domain.model.GnssCorsSite;
+import au.gov.ga.geodesy.domain.model.GnssCorsSiteRepository;
+import au.gov.ga.geodesy.domain.model.WeeklySolution;
+import au.gov.ga.geodesy.domain.model.WeeklySolutionAvailable;
+import au.gov.ga.geodesy.domain.model.WeeklySolutionRepository;
+
+@Component
+@Transactional("geodesyTransactionManager")
+public class PositionService implements EventSubscriber<WeeklySolutionAvailable> {
+
+    @Autowired
+    private WeeklySolutionRepository solutions;
+
+    @Autowired
+    private GnssCorsSiteRepository sites;
+
+    @Autowired
+    private EventPublisher eventPublisher;
+
+    @PostConstruct
+    public void subscribe() {
+        eventPublisher.subscribe(this);
+    }
+
+    public boolean canHandle(Event event) {
+        return event instanceof WeeklySolutionAvailable;
+    }
+
+    public void handle(WeeklySolutionAvailable event) {
+        WeeklySolution solution = solutions.findOne(event.getWeeklySolutionId());
+
+        File sinex = new File(solution.getSinexFile());
+        try (
+            Scanner scanner = new Scanner(sinex);
+        ) {
+            while (!scanner.nextLine().startsWith("+SOLUTION/ESTIMATE"));
+            scanner.nextLine();
+
+            String line = null;
+            while (!(line = scanner.nextLine()).startsWith("-SOLUTION/ESTIMATE")) {
+
+                Pair<String, BigDecimal> xLine = parseEstimatedValue(line);
+                Pair<String, BigDecimal> yLine = parseEstimatedValue(scanner.nextLine());
+                Pair<String, BigDecimal> zLine = parseEstimatedValue(scanner.nextLine());
+
+                double x = xLine.getRight().doubleValue();
+                double y = yLine.getRight().doubleValue();
+                double z = zLine.getRight().doubleValue();
+
+                GnssCorsSite site = sites.findByFourCharacterId(xLine.getLeft());
+
+                System.out.println(xLine.getLeft() + ": " + x + ", " + y + ", " + z);
+
+                /* Position p = new Position(site.getId(), null, 5332, x, y, z, solution.getEpoch(), new Date(), solution.getId()); */
+                /* positions.save(p); */
+            }
+            System.out.println("last line read is: " + line);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        eventPublisher.handled(event);
+    }
+
+    private Pair<String, BigDecimal> parseEstimatedValue(String line) {
+        StringTokenizer tokenizer = new StringTokenizer(line);
+
+        drop(tokenizer, 2);
+        String fourCharacterId = tokenizer.nextToken();
+
+        drop(tokenizer, 5);
+        BigDecimal value = new BigDecimal(tokenizer.nextToken());
+
+        return Pair.of(fourCharacterId, value);
+    }
+
+    private void drop(StringTokenizer tokenizer, int n) {
+        for (int i = 0; i < n; i++) {
+            tokenizer.nextToken();
+        }
+    }
+}

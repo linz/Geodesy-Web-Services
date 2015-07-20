839f3a30f895555aa6ef73490ea9a7c7c8a0bb51
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-07-21 09:51:41 +1000

    Export pageable setups/findBySiteId.

--- src/main/java/au/gov/ga/geodesy/domain/model/SetupRepository.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/SetupRepository.java
@@ -2,10 +2,16 @@ package au.gov.ga.geodesy.domain.model;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface SetupRepository extends JpaRepository<Setup, Integer>, SetupRepositoryCustom {
 
+    Page<Setup> findBySiteId(@Param("siteId") Integer id, Pageable pageRequest);
+
+    @RestResource(exported = false)
     List<Setup> findBySiteId(@Param("siteId") Integer id);
 }

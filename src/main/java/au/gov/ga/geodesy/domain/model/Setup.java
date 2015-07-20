1927ccfa13f2e9608a3f31fb5558c25f2b230eeb
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-07-20 14:20:14 +1000

    Begin to use eager JPA collections.

--- src/main/java/au/gov/ga/geodesy/domain/model/Setup.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/Setup.java
@@ -7,6 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -40,7 +41,7 @@ public class Setup {
     @Embedded
     public EffectiveDates effectivePeriod;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "SETUP_ID")
     private List<EquipmentInUse> equipmentInUse = new ArrayList<EquipmentInUse>();
 

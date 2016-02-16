624e57ba2634bb8da0a85826fd3ab8bb816a0815
Brooke Smith <brooke.smith@ga.gov.au>
2016-02-16 11:47:07 +1100

    #1 Update and create tests  * Improved existing service/ChangeReceiverAtABRKTest.java test  * Added service/MultipleSites.java test

--- src/test/java/au/gov/ga/geodesy/domain/service/ChangeReceiverAtABRKTest.java
+++ src/test/java/au/gov/ga/geodesy/domain/service/ChangeReceiverAtABRKTest.java
@@ -1,9 +1,9 @@
 package au.gov.ga.geodesy.domain.service;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
@@ -11,9 +11,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import au.gov.ga.geodesy.igssitelog.domain.model.IgsSiteLog;
+import au.gov.ga.geodesy.igssitelog.domain.model.IgsSiteLogRepository;
 import au.gov.ga.geodesy.igssitelog.interfaces.xml.IgsSiteLogXmlMarshaller;
 import au.gov.ga.geodesy.igssitelog.interfaces.xml.MarshallingException;
 import au.gov.ga.geodesy.support.spring.GeodesyServiceTestConfig;
@@ -33,12 +35,13 @@ public class ChangeReceiverAtABRKTest extends AbstractTransactionalTestNGSpringC
 
     @Autowired
     private IgsSiteLogXmlMarshaller marshaller;
+    
+    @Autowired
+    private IgsSiteLogRepository siteLogs;
 
     private void executeSiteLogScenario(String scenarioDirName) throws FileNotFoundException, MarshallingException {
-        File[] siteLogFiles = new File(scenarioDirName).listFiles(new FileFilter() {
-            public boolean accept(File f) {
-                return f.getName().endsWith(".xml");
-            }
+        File[] siteLogFiles = new File(scenarioDirName).listFiles((File dir, String f) -> {
+            return f.endsWith(".xml");
         });
         for (File siteLogFile : siteLogFiles) {
             IgsSiteLog siteLog = marshaller.unmarshal(new FileReader(siteLogFile));
@@ -49,7 +52,11 @@ public class ChangeReceiverAtABRKTest extends AbstractTransactionalTestNGSpringC
     @Test
     @Rollback(false)
     public void checkSetupId() throws Exception {
+        List<IgsSiteLog> siteLogMembers = siteLogs.findAll();
+        Assert.assertEquals(0, siteLogMembers.size());
         executeSiteLogScenario(scenarioDirName);
         // TODO: check
+        siteLogMembers = siteLogs.findAll();
+        Assert.assertEquals(1, siteLogMembers.size());
     }
 }
---
+++ src/test/java/au/gov/ga/geodesy/domain/service/MultipleSites.java
@@ -0,0 +1,73 @@
+package au.gov.ga.geodesy.domain.service;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.Test;
+
+import au.gov.ga.geodesy.igssitelog.domain.model.IgsSiteLog;
+import au.gov.ga.geodesy.igssitelog.domain.model.IgsSiteLogRepository;
+import au.gov.ga.geodesy.igssitelog.interfaces.xml.IgsSiteLogXmlMarshaller;
+import au.gov.ga.geodesy.igssitelog.interfaces.xml.MarshallingException;
+import au.gov.ga.geodesy.support.spring.GeodesyServiceTestConfig;
+import au.gov.ga.geodesy.support.spring.PersistenceJpaConfig;
+
+@ContextConfiguration(classes = { GeodesyServiceTestConfig.class,
+		PersistenceJpaConfig.class }, loader = AnnotationConfigContextLoader.class)
+
+@Transactional("geodesyTransactionManager")
+public class MultipleSites extends AbstractTransactionalTestNGSpringContextTests {
+
+	private static final String scenarioDirName = "src/test/resources/multiple-sites/";
+																	  
+	@Autowired
+	private IgsSiteLogService siteLogService;
+
+	@Autowired
+	private IgsSiteLogXmlMarshaller marshaller;
+
+	@Autowired
+	private IgsSiteLogRepository siteLogs;
+	
+	private int numberOfSites;
+
+	/**
+	 * Each siteLog file must have a unique getSiteIdentification.
+	 * @param scenarioDirName
+	 * @throws MarshallingException
+	 * @throws IOException 
+	 */
+	private void executeSiteLogScenario(String scenarioDirName) throws MarshallingException, IOException {
+		File[] siteLogFiles = new File(scenarioDirName).listFiles((File dir, String f) -> {
+			return f.endsWith(".xml");
+		});
+		numberOfSites = siteLogFiles.length;
+		for (File siteLogFile : siteLogFiles) {
+			IgsSiteLog siteLog = marshaller.unmarshal(new FileReader(siteLogFile));
+			siteLogService.upload(siteLog);
+		}
+	}
+
+	@Test
+	@Rollback(false)
+	public void checkSetupId() throws Exception {
+		List<IgsSiteLog> siteLogMembers = siteLogs.findAll();
+		Assert.assertEquals(0, siteLogMembers.size());
+		executeSiteLogScenario(scenarioDirName);
+		siteLogMembers = siteLogs.findAll();
+		Assert.assertEquals(numberOfSites, siteLogMembers.size());
+	}
+}

21fa54ee5e63bade880a39c4cb62b44c57496d97
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-03-22 16:34:02 +1100

    Map GeodestML address
    Former-commit-id: a61c9971db5465022b1f3d009a078ee323f3f10e

--- src/main/java/au/gov/ga/geodesy/support/mapper/orika/ResponsiblePartyOrikaMapper.java
+++ src/main/java/au/gov/ga/geodesy/support/mapper/orika/ResponsiblePartyOrikaMapper.java
@@ -2,6 +2,7 @@ package au.gov.ga.geodesy.support.mapper.orika;
 
 import org.geotools.metadata.iso.citation.ResponsiblePartyImpl;
 import org.geotools.util.SimpleInternationalString;
+import org.opengis.metadata.citation.Address;
 import org.opengis.metadata.citation.ResponsibleParty;
 import org.opengis.metadata.citation.Telephone;
 import org.opengis.util.InternationalString;
@@ -10,6 +11,7 @@ import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 
+import net.opengis.iso19139.gmd.v_20070417.CIAddressType;
 import net.opengis.iso19139.gmd.v_20070417.CIResponsiblePartyType;
 import net.opengis.iso19139.gmd.v_20070417.CITelephoneType;
 
@@ -24,10 +26,19 @@ public class ResponsiblePartyOrikaMapper {
         mapperFactory.classMap(Telephone.class, CITelephoneType.class)
             .field("voices", "voice")
             .field("facsimiles", "facsimile")
+            .byDefault()
             .register();
+
+        mapperFactory.classMap(Address.class, CIAddressType.class)
+            .field("deliveryPoints", "deliveryPoint")
+            .field("electronicMailAddresses", "electronicMailAddress")
+            .byDefault()
+            .register();
+
 
         mapperFactory.classMap(ResponsiblePartyImpl.class, CIResponsiblePartyType.class)
             .field("contactInfo", "contactInfo.CIContact")
+            .field("contactInfo.address", "contactInfo.CIContact.address.CIAddress")
             .field("contactInfo.phone", "contactInfo.CIContact.phone.CITelephone")
             .byDefault()
             .register();
--- src/test/java/au/gov/ga/geodesy/port/adapter/geodesyml/ResponsiblePartyMarshallingTest.java
+++ src/test/java/au/gov/ga/geodesy/port/adapter/geodesyml/ResponsiblePartyMarshallingTest.java
@@ -1,8 +1,13 @@
 package au.gov.ga.geodesy.port.adapter.geodesyml;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.geotools.metadata.iso.citation.AddressImpl;
+import org.geotools.metadata.iso.citation.ContactImpl;
 import org.geotools.metadata.iso.citation.ResponsiblePartyImpl;
+import org.geotools.metadata.iso.citation.TelephoneImpl;
 import org.geotools.util.SimpleInternationalString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -31,6 +36,23 @@ public class ResponsiblePartyMarshallingTest extends AbstractTestNGSpringContext
         party.setIndividualName("Lazar Bodor");
         party.setOrganisationName(new SimpleInternationalString("Geoscience Australia"));
         party.setPositionName(new SimpleInternationalString("software developer"));
+
+        ContactImpl contact = new ContactImpl();
+        TelephoneImpl telephone = new TelephoneImpl();
+        telephone.setVoices(new ArrayList<String>());
+        telephone.getVoices().add("04 51989787");
+        telephone.getVoices().add("04 75723066");
+        contact.setPhone(telephone);
+        party.setContactInfo(contact);
+
+        AddressImpl address = new AddressImpl();
+        address.setDeliveryPoints(Arrays.asList("12 Olivia Pl"));
+        address.setCity(new SimpleInternationalString("Bright"));
+        address.setCountry(new SimpleInternationalString("Australia"));
+        address.setPostalCode("2789");
+        address.setElectronicMailAddresses(Arrays.asList("a@gmail.com", "b@gmail.com"));
+        contact.setAddress(address);
+
 
         marshaller.marshal(mapper.mapToDto(party), new PrintWriter(System.out));
     }

84a19103a7fc25bb265f6442c803953849a05e38
Brooke Smith <brooke.smith@ga.gov.au>
2016-03-29 10:45:26 +1100

    Change default output location to resource dir and commit
    * Commit of translated files in separate commit
    
    
    Former-commit-id: 765348b9cbb1953ed3f47d20eb1876be64f6c457

--- runalltestinput.sh
+++ runalltestinput.sh
@@ -2,16 +2,11 @@
 # Run the translate over all inputfiles in src/test/resources/sitelog and output to /tmp/geodesyml/runall
 
 INPUTDIR=src/test/resources/sitelog
-OUTPUTDIR=/tmp/geodesyml/runall
+OUTPUTDIR=src/test/resources/sitelog/sitelogtoGeodesyML
+OUTPUTDIRTMP=/tmp/geodesyml/runall
 
 if [ ! -d $OUTPUTDIR ]; then
     mkdir -p $OUTPUTDIR
 fi
 
-#for i in $INPUTDIR/*
-#do
-#    echo $i
-    
 mvn exec:java -Dexec.mainClass="au.gov.ga.geodesy.support.commandline.GeodesyMLCommandLine" -Dexec.args="-indir $INPUTDIR -outdir $OUTPUTDIR"
-#    exit 1
-#done

adce1472efbede6b6c01d2f67620cff4bc9d54c1
simondobner <simon.dobner@ga.gov.au>
2016-04-06 12:02:31 +1000

    Unwrap another SQL exception
    Former-commit-id: 2f61c7fbe58c141722681ced695157ba064ce865

--- src/test/java/au/gov/ga/geodesy/support/testng/TestListener.java
+++ src/test/java/au/gov/ga/geodesy/support/testng/TestListener.java
@@ -1,6 +1,7 @@
 package au.gov.ga.geodesy.support.testng;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.orm.jpa.JpaSystemException;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -14,8 +15,9 @@ public class TestListener extends TestListenerAdapter implements ITestListener {
             ((DataIntegrityViolationException) x).getRootCause().printStackTrace();
         } else if (x instanceof JpaSystemException) {
             ((JpaSystemException) x).getRootCause().printStackTrace();
+        } else if (x instanceof InvalidDataAccessResourceUsageException) {
+            ((InvalidDataAccessResourceUsageException) x).getRootCause().printStackTrace();
         }
         super.onTestFailure(failure);
     }
 }
-

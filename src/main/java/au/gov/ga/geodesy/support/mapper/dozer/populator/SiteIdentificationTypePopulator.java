1494981c404235394dc3dd759b86281be1f85f4b
Brooke Smith <brooke.smith@ga.gov.au>
2016-04-11 20:27:52 +1000

    Fix site identification after validation failures
    Former-commit-id: 8eb6db19e74c5c1a72d7ef8f32ce90af6b7a4a4e

---
+++ src/main/java/au/gov/ga/geodesy/support/mapper/dozer/populator/SiteIdentificationTypePopulator.java
@@ -0,0 +1,24 @@
+package au.gov.ga.geodesy.support.mapper.dozer.populator;
+
+import au.gov.ga.geodesy.support.utils.GMLGmlTools;
+import au.gov.ga.geodesy.support.utils.GMLMiscTools;
+import au.gov.xml.icsm.geodesyml.v_0_3.SiteIdentificationType;
+
+/**
+ * The translate simply copied the Countries (in Sopac Sitelog XML) across to the GeoesyML CountryCode elements.
+ * This cleans those up by converting to the actual CountryCodes.
+ * 
+ * @author brookes
+ *
+ */
+public class SiteIdentificationTypePopulator extends GeodesyMLElementPopulator<SiteIdentificationType> {
+    /**
+     * Consider all required elements for this type and add any missing ones with default values.
+     * 
+     * @param gnssReceiverType
+     */
+    void checkAllRequiredElementsPopulated(SiteIdentificationType sensorType) {
+        checkElementPopulated(sensorType, "cdpNumber", GMLMiscTools.getEmptyString());
+        checkElementPopulated(sensorType, "iersDOMESNumber", GMLMiscTools.getEmptyString());
+    }
+}

43621797d1a786a23bd6377dbd5e2e74ea0d44a5
Brooke Smith <brooke.smith@ga.gov.au>
2016-04-13 10:58:04 +1000

    Fix missing required elements in GnssReceiverType
    Former-commit-id: 284541bc240ee5dfc0464dbb7f58487b2571512e

--- src/main/java/au/gov/ga/geodesy/support/mapper/dozer/populator/GnssReceiverTypePopulator.java
+++ src/main/java/au/gov/ga/geodesy/support/mapper/dozer/populator/GnssReceiverTypePopulator.java
@@ -3,6 +3,8 @@ package au.gov.ga.geodesy.support.mapper.dozer.populator;
 import au.gov.ga.geodesy.support.utils.GMLGmlTools;
 import au.gov.ga.geodesy.support.utils.GMLMiscTools;
 import au.gov.xml.icsm.geodesyml.v_0_3.GnssReceiverType;
+import au.gov.xml.icsm.geodesyml.v_0_3.IgsReceiverModelCodeType;
+import net.opengis.gml.v_3_2_1.CodeType;
 
 /**
  * The receivers have required elements that don't all exist in the SOPAC Sitelog xml. This fills them in.
@@ -23,6 +25,18 @@ public class GnssReceiverTypePopulator extends GeodesyMLElementPopulator<GnssRec
         // This element can be blank when receiver hasn't been removed. Some other logic in the project
         // removes empty elements from the Sopac SiteLog before it gets to this translator
         checkElementPopulated(gnssReceiverType, "dateRemoved", GMLGmlTools.getEmptyTimePositionType());
+        checkElementPopulated(gnssReceiverType, "dateInstalled", GMLGmlTools.getEmptyTimePositionType());
+        checkElementPopulated(gnssReceiverType, "dateRemoved", GMLGmlTools.getEmptyTimePositionType());
+        checkElementPopulated(gnssReceiverType, "receiverType", getEmptyIgsReceiverModelCodeType());
+        
+        // checkElementPopulated(gnssReceiverType, "satelliteSystem",  ... );
+        // The checkElementPopulated() for "satelliteSystem" doesnt help here as GnssReceiverType.getSatelliteSystem() creates an empty List
+        // if it is null. And the moxy marshalling is currently configured to remove any empty lists. So add in a satelliteSystem with dummy
+        // CodeType.
+        if (gnssReceiverType.getSatelliteSystem().size() == 0) {
+            gnssReceiverType.setSatelliteSystem(GMLMiscTools.getEmptyList(CodeType.class, getCodeType()));
+        }
+        checkElementPopulated(gnssReceiverType, "firmwareVersion", GMLMiscTools.getEmptyString());
 
         // GnssReceiverType has a SerialNumber (defined on it) and a manufacturerModel (defined on BaseGeodeticEquipmentType, which is the
         // Grandparent of) and we are uncertain exactly how to handle this multiplicity of what seems to be an element with the same
@@ -35,5 +49,21 @@ public class GnssReceiverTypePopulator extends GeodesyMLElementPopulator<GnssRec
         }
         checkElementPopulated(gnssReceiverType, "serialNumber", gnssReceiverType.getManufacturerSerialNumber());
 
+    }
+
+    private CodeType getCodeType() {
+        CodeType codeType = GMLGmlTools.getEmptyCodeType();
+        codeType.setValue("DummyCodeList");
+        codeType.setCodeSpace("DummyCodeSpace");
+        return codeType;
+    }
+
+    private IgsReceiverModelCodeType getEmptyIgsReceiverModelCodeType() {
+        IgsReceiverModelCodeType igsReceiverModelCodeType = new IgsReceiverModelCodeType();
+        // TODO fix this CodeLit
+        igsReceiverModelCodeType.setCodeList("DummyCodeList");
+        igsReceiverModelCodeType.setCodeListValue("DummyCodeListValue");
+        igsReceiverModelCodeType.setCodeSpace("DummyCodeSpace");
+        return igsReceiverModelCodeType;
     }
 }

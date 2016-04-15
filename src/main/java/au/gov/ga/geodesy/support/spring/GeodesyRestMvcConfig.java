ced6c6b995f7755bd2d9b2cacacda68d1db42b18
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-04-15 16:11:01 +1000

    Pretty-print JSON
    Former-commit-id: a226f44ea92925847962c1ba19605917ce61996c

--- src/main/java/au/gov/ga/geodesy/support/spring/GeodesyRestMvcConfig.java
+++ src/main/java/au/gov/ga/geodesy/support/spring/GeodesyRestMvcConfig.java
@@ -1,12 +1,40 @@
 package au.gov.ga.geodesy.support.spring;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"au.gov.ga.geodesy.port.adapter.rest"})
 public class GeodesyRestMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper());
+        return mappingJackson2HttpMessageConverter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objMapper = new ObjectMapper();
+        objMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objMapper;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
 }

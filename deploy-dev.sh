c92a520878dd7224db63e6867065e11b4da0d2ed
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-05-06 14:52:41 +1000

    Set environment name to D for dev

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export AWS_PROFILE=geodesy
 PATH=~/.local/bin:$PATH
-ENV=A GEODESY_WEB_SERVICES_VERSION=1.0.0-SNAPSHOT make clean stack
+ENV=D GEODESY_WEB_SERVICES_VERSION=1.0.0-SNAPSHOT make clean stack

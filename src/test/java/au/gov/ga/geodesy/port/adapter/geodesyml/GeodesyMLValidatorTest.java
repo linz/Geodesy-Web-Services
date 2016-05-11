0c3c3f77c18d00b1d020e551173de0d64fc34813
heyagosper <heya.gosper@ga.gov.au>
2016-05-11 16:11:06 +1000

    fix unit test
    Former-commit-id: ee2edd8ffc02aaead9f89ce657cebff1a6eed539

--- src/test/java/au/gov/ga/geodesy/port/adapter/geodesyml/GeodesyMLValidatorTest.java
+++ src/test/java/au/gov/ga/geodesy/port/adapter/geodesyml/GeodesyMLValidatorTest.java
@@ -1,24 +1,52 @@
 package au.gov.ga.geodesy.port.adapter.geodesyml;
 
 import au.gov.ga.xmlschemer.Violation;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
 public class GeodesyMLValidatorTest {
 
-    // TODO fix the constructor so that the unit tests will pass
-    private GeodesyMLValidator validator = new GeodesyMLValidator(null, "");
+    private GeodesyMLValidator geodesyMLValidator = null;
+
+    @BeforeTest
+    public void setup() {
+        ClassPathXmlApplicationContext context = null;
+        try {
+            context = new ClassPathXmlApplicationContext();
+            Resource catalogResource = context.getResource("classpath:xsd/geodesyml-1.0.0-SNAPSHOT/catalog.xml");
+            Resource xsdResource = context.getResource("classpath:xsd/geodesyml-1.0.0-SNAPSHOT/geodesyML.xsd");
+            Source xsdSource = new StreamSource(xsdResource.getInputStream());
+            String catalogPath = catalogResource.getFile().getAbsolutePath();
+
+            geodesyMLValidator = new GeodesyMLValidator(xsdSource, catalogPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void testSchemaValidation() throws Exception {
         StreamSource xml = new StreamSource(ResourceUtils.getFile("classpath:sitelog/geodesyml/MOBS.xml"));
-        List<Violation> violations = validator.validate(xml);
+        List<Violation> violations = geodesyMLValidator.validate(xml);
         assertNoViolations(violations);
+    }
+
+    @Test
+    public void testSchemaValidationFailure() throws Exception {
+        // load a file with 2 known schema violations
+        StreamSource xml = new StreamSource(ResourceUtils.getFile("classpath:sitelog/geodesyml/MOBS-invalid-schema.xml"));
+        List<Violation> violations = geodesyMLValidator.validate(xml);
+        assertViolations(violations, 2);
     }
 
     private void assertNoViolations(List<Violation> violations) throws AssertionError {

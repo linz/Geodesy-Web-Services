8fe8d2072908fe3ab422f14ec579474d658c277b
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-05-13 10:48:43 +1000

    Register a test instance of GeodesyMLValidator
    Former-commit-id: 788cd7be07930710235ca64b979715a830a1aaa4

--- src/test/java/au/gov/ga/geodesy/support/spring/GeodesyServiceTestConfig.java
+++ src/test/java/au/gov/ga/geodesy/support/spring/GeodesyServiceTestConfig.java
@@ -1,10 +1,14 @@
 package au.gov.ga.geodesy.support.spring;
 
+import java.io.FileNotFoundException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import au.gov.ga.geodesy.domain.model.SynchronousEventPublisher;
 import au.gov.ga.geodesy.domain.model.event.EventPublisher;
+import au.gov.ga.geodesy.port.adapter.geodesyml.GeodesyMLValidator;
 
 @Configuration
 public class GeodesyServiceTestConfig extends GeodesyServiceConfig {
@@ -13,5 +17,11 @@ public class GeodesyServiceTestConfig extends GeodesyServiceConfig {
     @Override
     public EventPublisher eventPublisher() {
         return new SynchronousEventPublisher();
+    }
+
+    @Bean
+    public GeodesyMLValidator getGeodesyMLValidator() throws FileNotFoundException {
+        String catalog = ResourceUtils.getFile("classpath:xsd/geodesyml-1.0.0-SNAPSHOT/catalog.xml").getAbsolutePath();
+        return new GeodesyMLValidator(catalog);
     }
 }

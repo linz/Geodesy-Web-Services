89aace7256ecf152d229bc4d7dafc99bb621a189
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-05-13 12:32:34 +1000

    Use Orika's default compiler strategy
    Former-commit-id: b8b363c5f95f1ed69dbeba6e8331fdd3727250b5

--- src/main/java/au/gov/ga/geodesy/support/mapper/orika/sopac/SiteLogSopacOrikaMapper.java
+++ src/main/java/au/gov/ga/geodesy/support/mapper/orika/sopac/SiteLogSopacOrikaMapper.java
@@ -1,11 +1,5 @@
 package au.gov.ga.geodesy.support.mapper.orika.sopac;
 
-import org.geotools.metadata.iso.citation.ContactImpl;
-import org.geotools.metadata.iso.citation.ResponsiblePartyImpl;
-import org.geotools.metadata.iso.citation.TelephoneImpl;
-import org.opengis.metadata.citation.Telephone;
-import org.springframework.stereotype.Component;
-
 import au.gov.ga.geodesy.domain.model.sitelog.SiteLog;
 import au.gov.ga.geodesy.igssitelog.domain.model.Agency;
 import au.gov.ga.geodesy.igssitelog.domain.model.IgsSiteLog;
@@ -16,37 +10,49 @@ import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
+import org.geotools.metadata.iso.citation.ContactImpl;
+import org.geotools.metadata.iso.citation.ResponsiblePartyImpl;
+import org.geotools.metadata.iso.citation.TelephoneImpl;
+import org.opengis.metadata.citation.Telephone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Component
 public class SiteLogSopacOrikaMapper implements SopacSiteLogMapper {
-
+    private Logger logger = LoggerFactory.getLogger(SiteLogSopacOrikaMapper.class);
     private MapperFacade mapper;
 
     public SiteLogSopacOrikaMapper() {
-        MapperFactory factory = new DefaultMapperFactory.Builder()
-            .compilerStrategy(new EclipseJdtCompilerStrategy())
-            .build();
+        MapperFactory factory;
+
+        if (logger.isDebugEnabled()) {
+            // This configuration will dump the generated java files to the target directory
+            factory = new DefaultMapperFactory.Builder().compilerStrategy(new EclipseJdtCompilerStrategy()).build();
+        } else {
+            factory = new DefaultMapperFactory.Builder().build();
+        }
 
         factory.classMap(IgsSiteLog.class, SiteLog.class)
-            .field("contactAgency", "siteContact.party")
-            .field("responsibleAgency", "siteMetadataCustodian.party")
-            .exclude("equipmentLogItems")
-            .byDefault()
-            .register();
+                .field("contactAgency", "siteContact.party")
+                .field("responsibleAgency", "siteMetadataCustodian.party")
+                .exclude("equipmentLogItems")
+                .byDefault()
+                .register();
 
         factory.classMap(Agency.class, ResponsiblePartyImpl.class)
-            .field("name", "organisationName")
-            .field("primaryContact.name", "individualName")
-            .field("primaryContact", "contactInfo")
-            .byDefault()
-            .register();
+                .field("name", "organisationName")
+                .field("primaryContact.name", "individualName")
+                .field("primaryContact", "contactInfo")
+                .byDefault()
+                .register();
 
         factory.classMap(au.gov.ga.geodesy.igssitelog.domain.model.Contact.class, ContactImpl.class)
-            .fieldMap("telephonePrimary", "phone.voices.fst:{|add(%s)}").mapNulls(false).add()
-            .fieldMap("telephoneSecondary", "phone.voices.fst:{|add(%s)}").mapNulls(false).add()
-            .fieldMap("fax", "phone.facsimiles.fst:{|add(%s)}").mapNulls(false).add()
-            .byDefault()
-            .register();
+                .fieldMap("telephonePrimary", "phone.voices.fst:{|add(%s)}").mapNulls(false).add()
+                .fieldMap("telephoneSecondary", "phone.voices.fst:{|add(%s)}").mapNulls(false).add()
+                .fieldMap("fax", "phone.facsimiles.fst:{|add(%s)}").mapNulls(false).add()
+                .byDefault()
+                .register();
 
         factory.getConverterFactory().registerConverter(new StringToInternationalStringConverter());
         factory.getConverterFactory().registerConverter(new StringToStringPropertyConverter());

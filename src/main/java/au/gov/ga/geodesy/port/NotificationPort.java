c90d1ee64bbc58fe1ecf2852ba7bf4c1d894553a
Brooke Smith <brooke.smith@ga.gov.au>
2016-05-17 12:55:07 +1000

    Revert "Revert "Merge pull request #110 from GeoscienceAustralia/68-email-notifications""
    This reverts commit 6490e590cab8fafafccad28d7a5eb2464ef92ffe [formerly 6c235a50e5460595f45b5f361a82e576b4c5404c].
    
    
    Former-commit-id: 76e6e2ad06c16b346393cb41bedebdad0d0f28d9

---
+++ src/main/java/au/gov/ga/geodesy/port/NotificationPort.java
@@ -0,0 +1,7 @@
+package au.gov.ga.geodesy.port;
+
+import au.gov.ga.geodesy.domain.model.event.Event;
+
+public interface NotificationPort {
+    public void sendNotification(Event e);
+}
---
+++ src/test/java/au/gov/ga/geodesy/domain/service/NotificationServiceTest.java
@@ -0,0 +1,159 @@
+package au.gov.ga.geodesy.domain.service;
+
+import java.io.IOException;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
+import au.gov.ga.geodesy.domain.model.event.InvalidSiteLogReceived;
+import au.gov.ga.geodesy.domain.model.event.SiteLogReceived;
+import au.gov.ga.geodesy.domain.model.event.SiteUpdated;
+import au.gov.ga.geodesy.domain.model.event.WeeklySolutionAvailable;
+import au.gov.ga.geodesy.port.adapter.smtp.SMTPNoficationAdapter;
+import au.gov.ga.geodesy.support.email.SpringMailAdapter;
+import au.gov.ga.geodesy.support.properties.GeodesyMailConfig;
+import au.gov.ga.geodesy.support.properties.GeodesyNotificationsConfig;
+import au.gov.ga.geodesy.support.spring.TestAppConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestAppConfig.class, SpringMailAdapter.class, SMTPNoficationAdapter.class,
+        NotificationService.class, GeodesyMailConfig.class, GeodesyNotificationsConfig.class})
+public class NotificationServiceTest {
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private JavaMailSenderImpl emailSender;
+
+    @Autowired
+    private GeodesyNotificationsConfig geodesyNotificationsConfig;
+
+    private GreenMail testSMTP;
+
+    @Before
+    public void begin() {
+        testSMTP = new GreenMail(ServerSetupTest.SMTP);
+        testSMTP.start();
+
+        emailSender.setPort(3025);
+        emailSender.setHost("localhost");
+    }
+
+    @After
+    public void end() {
+        testSMTP.stop();
+    }
+
+    @Test
+    public void testNotificationServiceInvalidSiteLogReceived() throws MessagingException, IOException {
+        String content = "Sitelog text";
+        InvalidSiteLogReceived event = new InvalidSiteLogReceived(content);
+        int numberMsgEqualNumberTos = geodesyNotificationsConfig.getInvalidSiteLogReceivedEmailAddressees().size();
+        String fromConfig = geodesyNotificationsConfig.getFromEmail();
+        notificationService.handle(event);
+
+        // Check emails
+
+        Message[] messages = testSMTP.getReceivedMessages();
+        Assert.assertEquals(numberMsgEqualNumberTos, messages.length);
+        for (Message message : messages) {
+            Address from = message.getFrom()[0];
+            Assert.assertEquals(fromConfig, from.toString());
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(event.getMessage()));
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(content));
+
+            System.out.println("Message: " + message.getContent());
+        }
+    }
+
+    @Test
+    public void testNotificationServiceSiteLogReceived() throws MessagingException, IOException {
+        String fourCharID = "ALIC";
+        SiteLogReceived event = new SiteLogReceived(fourCharID);
+        int numberMsgEqualNumberTos = geodesyNotificationsConfig.getInvalidSiteLogReceivedEmailAddressees().size();
+        String fromConfig = geodesyNotificationsConfig.getFromEmail();
+        notificationService.handle(event);
+
+        // Check emails
+
+        Message[] messages = testSMTP.getReceivedMessages();
+        Assert.assertEquals(numberMsgEqualNumberTos, messages.length);
+        for (Message message : messages) {
+            Address from = message.getFrom()[0];
+            Assert.assertEquals(fromConfig, from.toString());
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(event.getMessage()));
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(fourCharID));
+
+            System.out.println("Message: " + message.getContent());
+        }
+    }
+
+    @Test
+    public void testNotificationServiceSiteUpdated() throws MessagingException, IOException {
+        String fourCharID = "ALIC";
+        SiteUpdated event = new SiteUpdated(fourCharID);
+        int numberMsgEqualNumberTos = geodesyNotificationsConfig.getInvalidSiteLogReceivedEmailAddressees().size();
+        String fromConfig = geodesyNotificationsConfig.getFromEmail();
+        notificationService.handle(event);
+
+        // Check emails
+
+        Message[] messages = testSMTP.getReceivedMessages();
+        Assert.assertEquals(numberMsgEqualNumberTos, messages.length);
+        for (Message message : messages) {
+            Address from = message.getFrom()[0];
+            Assert.assertEquals(fromConfig, from.toString());
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(event.getMessage()));
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(fourCharID));
+
+            System.out.println("Message: " + message.getContent());
+        }
+    }
+
+    @Test
+    public void testNotificationServiceWeeklySolutionAvailable() throws MessagingException, IOException {
+        Integer weeklySolutionId = 123456789;
+        WeeklySolutionAvailable event = new WeeklySolutionAvailable(weeklySolutionId);
+        int numberMsgEqualNumberTos = geodesyNotificationsConfig.getInvalidSiteLogReceivedEmailAddressees().size();
+        String fromConfig = geodesyNotificationsConfig.getFromEmail();
+        notificationService.handle(event);
+
+        // Check emails
+
+        Message[] messages = testSMTP.getReceivedMessages();
+        Assert.assertEquals(numberMsgEqualNumberTos, messages.length);
+        for (Message message : messages) {
+            Address from = message.getFrom()[0];
+            Assert.assertEquals(fromConfig, from.toString());
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(event.getMessage()));
+            MatcherAssert.assertThat("the message content", (String) message.getContent(),
+                    Matchers.containsString(weeklySolutionId.toString()));
+
+            System.out.println("Message: " + message.getContent());
+        }
+    }
+
+}

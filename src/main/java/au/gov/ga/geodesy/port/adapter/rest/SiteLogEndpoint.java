325ef5419aab20d05f284b011f7ca771734faa74
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-05-19 08:44:02 +1000

    Http 201 for valid site logs, 400 otherwise
    Former-commit-id: 022b157356c81aeff7e3efb8526226b090bd4fad

--- src/main/java/au/gov/ga/geodesy/port/adapter/rest/SiteLogEndpoint.java
+++ src/main/java/au/gov/ga/geodesy/port/adapter/rest/SiteLogEndpoint.java
@@ -2,19 +2,26 @@ package au.gov.ga.geodesy.port.adapter.rest;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import au.gov.ga.geodesy.domain.service.IgsSiteLogService;
 import au.gov.ga.geodesy.port.InvalidSiteLogException;
@@ -27,7 +34,6 @@ import au.gov.ga.xmlschemer.Violation;
 @RequestMapping("/siteLog")
 public class SiteLogEndpoint {
 
-    @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(SiteLogEndpoint.class);
 
     @Autowired
@@ -48,14 +54,23 @@ public class SiteLogEndpoint {
     }
 
     @RequestMapping(value = "/sopac/upload", method = RequestMethod.POST)
-    public void upload(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
-        SiteLogReader reader = new SopacSiteLogReader(new InputStreamReader(req.getInputStream()));
+    public ResponseEntity<String> upload(HttpServletRequest req, HttpServletResponse rsp) throws IOException, InvalidSiteLogException {
+        String siteLogText = IOUtils.toString(req.getInputStream());
+        log.debug("Received SOPAC site log: " + siteLogText);
+        SiteLogReader reader = new SopacSiteLogReader(new StringReader(siteLogText));
         try {
             service.upload(reader.getSiteLog());
+            // TODO: return a URI to the created resource
+            return ResponseEntity.created(new URI("/siteLog/X")).body("");
         }
-        catch (InvalidSiteLogException e) {
-            // TODO
-            e.printStackTrace();
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidSiteLogException.class)
+    public void invalidSiteLogHandler(InvalidSiteLogException e) {
+        log.error("Received invalid site log", e);
     }
 }
---
+++ src/test/java/au/gov/ga/geodesy/port/adapter/rest/UploadInvalidSiteLogRestTest.java
@@ -0,0 +1,16 @@
+package au.gov.ga.geodesy.port.adapter.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.http.MediaType;
+import org.testng.annotations.Test;
+
+public class UploadInvalidSiteLogRestTest extends RestTest {
+
+    @Test
+    public void uploadInvalidSopacSiteLog() throws Exception {
+        mvc.perform(post("/siteLog/sopac/upload").contentType(MediaType.APPLICATION_XML).content("Invalid site log content"))
+            .andExpect(status().isBadRequest());
+    }
+}

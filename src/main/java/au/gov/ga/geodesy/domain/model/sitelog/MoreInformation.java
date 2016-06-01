f5d067252b7ca05289ec15cec50595a44b486520
Frank Fu <frank.fu@ga.gov.au>
2016-06-01 10:31:53 +1000

    Revert "Added a mapper for moreInformation element."
    This reverts commit 40093b34994139eb96820893b10b622453bcfaa8 [formerly bc42d30f6fdf65eca3042df164ca2e40026ea60d].
    
    
    Former-commit-id: a9fdb78d8212b9e85da1569e31f6779edf4c7253

--- src/main/java/au/gov/ga/geodesy/domain/model/sitelog/MoreInformation.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/sitelog/MoreInformation.java
@@ -56,10 +56,6 @@ public class MoreInformation {
     @Column(name = "MI_TEXT_GRAPHICS_FROM_ANTENNA", length = 256)
     protected String insertTextGraphicFromAntenna;
 
-    @Size(max = 256)
-    @Column(name = "MI_DIGITAL_OBJECT_ID", length = 256)
-    protected String doi;
-	
     /**
      * Return primary data center.
      */
@@ -230,19 +226,5 @@ public class MoreInformation {
      */
     public void setInsertTextGraphicFromAntenna(String value) {
         this.insertTextGraphicFromAntenna = value;
-    }
-	
-    /**
-     * Return insert digital object ID.
-     */
-    public String getDoi() {
-        return doi;
-    }
-
-    /**
-     * Set insert digital object ID.
-     */
-    public void setDoi(String value) {
-        this.doi = value;
     }
 }

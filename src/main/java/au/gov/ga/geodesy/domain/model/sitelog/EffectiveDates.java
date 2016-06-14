8118b41184efde5e7c5f79caaf00b7d35db50182
heyagosper <heya.gosper@ga.gov.au>
2016-06-14 13:05:23 +1000

    Remove unneeded import
    Former-commit-id: 8e710197950ae94ed022837647ae2049c9da62bb

--- src/main/java/au/gov/ga/geodesy/domain/model/sitelog/EffectiveDates.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/sitelog/EffectiveDates.java
@@ -5,8 +5,6 @@ import java.time.Instant;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
-import static sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte1.other;
-
 /**
  * http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/equipment/2004/baseEquipmentLib.xsd:baseSensorEquipmentType.effectiveDates Note: this is an attempt to
  * interpret date values as java dates, rather than as strings as defined in the SOPAC schema (feel free to revert to java strings, if this starts to cause

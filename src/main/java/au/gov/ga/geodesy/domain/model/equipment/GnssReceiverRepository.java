00f98a28b28d08135daf476aa478aa876b2ff0f2
Lazar Bodor <lazar.bodor@gmail.com>
2016-07-06 17:18:47 +1000

    GEOD-46 Remove custom GnssReceiver endpoint
    The default behaviour gives us find-by-example out of the box. All
    JpaRepositories implement QueryByExampleExecutor and spring data rest
    exports the required endpoint functionality automatically.
    
    
    Former-commit-id: 5e7ae094dbc4fc673a37f1dab5728e68f5890f80

--- src/main/java/au/gov/ga/geodesy/domain/model/equipment/GnssReceiverRepository.java
+++ src/main/java/au/gov/ga/geodesy/domain/model/equipment/GnssReceiverRepository.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 public interface GnssReceiverRepository extends JpaRepository<GnssReceiver, Integer>,
-    QueryByExampleExecutor<GnssReceiver>, QueryDslPredicateExecutor<GnssReceiver> {
+    QueryDslPredicateExecutor<GnssReceiver> {
 
     @Override
     List<GnssReceiver> findAll();

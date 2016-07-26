f005fcc63eb96508cc6b4dd952cfe7424451618f
Lazar Bodor <lazar.bodor@ga.com>
2016-07-26 13:17:52 +1000

    GEOD-92 Factor out rest doc configuration
    Former-commit-id: 7d71952885c2a0026f11a2c9e4ba64bc381473d3

---
+++ src/test/java/au/gov/ga/geodesy/port/adapter/rest/RestDocTest.java
@@ -0,0 +1,33 @@
+package au.gov.ga.geodesy.port.adapter.rest;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+import java.lang.reflect.Method;
+
+import org.springframework.restdocs.ManualRestDocumentation;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/*
+ * Spring RestDoc base class.
+ */
+public class RestDocTest extends RestTest {
+
+    private final ManualRestDocumentation restDocumentation = new ManualRestDocumentation("target/generated-snippets");
+
+    @BeforeMethod
+    public void setUp(Method method) {
+        mvc = MockMvcBuilders.webAppContextSetup(super.webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation)
+                .uris().withHost("egeodesy.ga.gov.au").withPort(80))
+            .build();
+
+        restDocumentation.beforeTest(getClass(), method.getName());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        restDocumentation.afterTest();
+    }
+}

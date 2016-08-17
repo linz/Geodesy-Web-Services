b8a120f6d4fbe2a9d71215f84442bd3455578952
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-08-17 10:59:18 +1000

    Remove old amazonia subtree

--- amazonia/amazonia/__init__.py
+++
@@ -1,7 +0,0 @@
-#pylint: disable=wildcard-import
-
-"""GA Amazonia Package that generates GA cloud formation templates"""
-
-from amazonia.amazonia_resources import *
-from amazonia.cftemplates import *
-

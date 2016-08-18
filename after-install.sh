0aefcbb5892082f57cf1790dba05f0b04e42c18d
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-08-18 16:57:14 +1000

    Adjust codedeploy database credentials

--- after-install.sh
+++ after-install.sh
@@ -13,11 +13,14 @@ else
     ENV=$(echo $DEPLOYMENT_GROUP_NAME | cut -c1-4)
 fi
 
-# Set Password
-RDS_APP_PASSWORD_KEY="${ENV^}"GeodesyRdsPassword
-NEW_GEODESY_PASSWORD=$(credstash get ${RDS_APP_PASSWORD_KEY})
-sed -i 's/${geodesy-db-password}/'"${NEW_GEODESY_PASSWORD}/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
-
-# Set DB
+# Set database endpoint
 RDS_ENDPOINT="${ENV^}"geodesyrds.geodesy.ga.gov.au
 sed -i 's/${geodesy-db-url}/jdbc:postgresql:\/\/'"${RDS_ENDPOINT}\/geodesy/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
+
+# Set database login
+DB_USERNAME_KEY="${ENV^}"GeodesyDbUsername
+DB_PASSWORD_KEY="${ENV^}"GeodesyDbPassword
+DB_USERNAME=$(credstash get ${DB_USERNAME_KEY})
+DB_PASSWORD=$(credstash get ${DB_PASSWORD_KEY})
+sed -i 's/${geodesy-db-username}/'"${DB_USERNAME}/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
+sed -i 's/${geodesy-db-password}/'"${DB_PASSWORD}/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml

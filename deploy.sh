d75ee208ada1c161676e93540ac7cfcd2280be34
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-08-18 16:12:33 +1000

    Rename database usernames and password keys

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,26 @@ AWS_PROFILE=geodesy
 ENV_FILE=infra-${ENV}.yaml
 STACK_NAME="${ENV^}"Geodesy
 
-RDS_MASTER_USERNAME_KEY=${STACK_NAME}GeodesyRdsUsername
-RDS_MASTER_PASSWORD_KEY=${STACK_NAME}GeodesyRdsPassword
+RDS_MASTER_USERNAME_KEY=${STACK_NAME}DbRdsUsername
+RDS_MASTER_PASSWORD_KEY=${STACK_NAME}DbRdsPassword
+
+DB_USERNAME_KEY=${STACK_NAME}DbUsername
+DB_PASSWORD_KEY=${STACK_NAME}DbPassword
 
 case ${COMMAND} in
 "create")
     RDS_MASTER_USERNAME=postgres
     RDS_MASTER_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
+
+    DB_USERNAME=geodesy
+    DB_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
     ;;
 "update")
     RDS_MASTER_USERNAME=$(credstash -p ${AWS_PROFILE} get ${RDS_MASTER_USERNAME_KEY})
     RDS_MASTER_PASSWORD=$(credstash -p ${AWS_PROFILE} get ${RDS_MASTER_PASSWORD_KEY})
+
+    DB_USERNAME=$(credstash -p ${AWS_PROFILE} get ${DB_USERNAME_KEY})
+    DB_PASSWORD=$(credstash -p ${AWS_PROFILE} get ${DB_PASSWORD_KEY})
     ;;
 *)
     echo "Usage: deploy-dev.sh (create|update) (dev|test|prod)"
@@ -30,10 +39,12 @@ python3 amazonia/amazonia/amz.py -y infra.yaml -d geodesy_ga_defaults.yaml -t in
 
 aws --profile ${AWS_PROFILE} cloudformation ${COMMAND}-stack --stack-name ${STACK_NAME} --template-body file://infra.json --parameters \
     ParameterKey=${RDS_MASTER_USERNAME_KEY},ParameterValue=${RDS_MASTER_USERNAME} \
-    ParameterKey=${RDS_MASTER_USERNAME_KEY},ParameterValue=${RDS_MASTER_PASSWORD}
+    ParameterKey=${RDS_MASTER_PASSWORD_KEY},ParameterValue=${RDS_MASTER_PASSWORD}
 
 if (( $? == 0 && ${COMMAND} == "create" )); then
     credstash -p ${AWS_PROFILE} put -a ${RDS_MASTER_USERNAME_KEY} ${RDS_MASTER_USERNAME}
     credstash -p ${AWS_PROFILE} put -a ${RDS_MASTER_PASSWORD_KEY} ${RDS_MASTER_PASSWORD}
-fi
 
+    credstash -p ${AWS_PROFILE} put -a ${DB_USERNAME_KEY} ${DB_USERNAME}
+    credstash -p ${AWS_PROFILE} put -a ${DB_PASSWORD_KEY} ${DB_PASSWORD}
+fi

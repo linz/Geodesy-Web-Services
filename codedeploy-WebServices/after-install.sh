3834e4b20c5bc39c900a8eaaf108113413f72020
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-08-21 18:59:59 +1000

    Set credstash path and region

--- codedeploy-WebServices/after-install.sh
+++ codedeploy-WebServices/after-install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Unpack WAR file
-mkdir /usr/share/tomcat8/webapps/ROOT
-unzip ROOT.war -d /usr/share/tomcat8/webapps/ROOT
 
 # Obtain Env
 DEPLOYMENT_GROUP_NAME_CUT=$(echo "${DEPLOYMENT_GROUP_NAME}" | cut -c1-3)
@@ -13,15 +10,27 @@ else
     ENV=$(echo $DEPLOYMENT_GROUP_NAME | cut -c1-4)
 fi
 
-# Set database endpoint
-RDS_ENDPOINT="${ENV^}"geodesyrds.geodesy.ga.gov.au
-sed -i 's/${geodesy-db-url}/jdbc:postgresql:\/\/'"${RDS_ENDPOINT}\/geodesy/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
+# TODO: Why is the default region not Sydney? How do we pull in the correct region?
+CREDSTASH="/usr/local/bin/credstash -r ap-southeast-2"
 
-# Set database login
+cd /usr/share/tomcat8/webapps
+
+# deploy new application
+rm -rf ROOT*
+mv geodesy-web-services.war ROOT.war
+unzip ROOT.war -d ROOT
+
+# set database endpoint
+# RDS_ENDPOINT="${ENV^}"geodesyrds.geodesy.ga.gov.au
+# temporarily hard-coded to the dev database
+RDS_ENDPOINT=dd1iyix40zjic7t.cxm7lrsl3bbf.ap-southeast-2.rds.amazonaws.com
+sed -i 's/${geodesy-db-url}/jdbc:postgresql:\/\/'"${RDS_ENDPOINT}\/geodesy/" ROOT/META-INF/context.xml
+
+# set database login
 DB_USERNAME_KEY="${ENV^}"GeodesyDbUsername
 DB_PASSWORD_KEY="${ENV^}"GeodesyDbPassword
-DB_USERNAME=$(credstash get ${DB_USERNAME_KEY})
-DB_PASSWORD=$(credstash get ${DB_PASSWORD_KEY})
-sed -i 's/${geodesy-db-username}/'"${DB_USERNAME}/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
-sed -i 's/${geodesy-db-password}/'"${DB_PASSWORD}/" /usr/share/tomcat8/webapps/ROOT/META-INF/context.xml
+DB_USERNAME=$(${CREDSTASH} get ${DB_USERNAME_KEY})
+DB_PASSWORD=$(${CREDSTASH} get ${DB_PASSWORD_KEY})
+sed -i 's/${geodesy-db-username}/'"${DB_USERNAME}/" ROOT/META-INF/context.xml
+sed -i 's/${geodesy-db-password}/'"${DB_PASSWORD}/" ROOT/META-INF/context.xml
 

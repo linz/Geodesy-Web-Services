af7767d2f82e4f275006b5c20baac9b42788bbad
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-08-21 20:30:40 +1000

    Factor out RDS endpoint

--- codedeploy-WebServices/after-install.sh
+++ codedeploy-WebServices/after-install.sh
@@ -9,10 +9,6 @@ rm -rf ROOT*
 mv geodesy-web-services.war ROOT.war
 unzip ROOT.war -d ROOT
 
-# set database endpoint
-# RDS_ENDPOINT="${ENV^}"geodesyrds.geodesy.ga.gov.au
-# temporarily hard-coded to the dev database
-RDS_ENDPOINT=dd1iyix40zjic7t.cxm7lrsl3bbf.ap-southeast-2.rds.amazonaws.com
 sed -i 's/${geodesy-db-url}/jdbc:postgresql:\/\/'"${RDS_ENDPOINT}\/GeodesyDb/" ROOT/META-INF/context.xml
 
 # set database login
--- codedeploy-WebServices/env.sh
+++ codedeploy-WebServices/env.sh
@@ -8,6 +8,11 @@ else
     ENV=$(echo $DEPLOYMENT_GROUP_NAME | cut -c1-4)
 fi
 
+# set database endpoint
+# RDS_ENDPOINT="${ENV^}"geodesyrds.geodesy.ga.gov.au
+# temporarily hard-coded to the dev database
+RDS_ENDPOINT=dd1iyix40zjic7t.cxm7lrsl3bbf.ap-southeast-2.rds.amazonaws.com
+
 # TODO: Why is the default region not Sydney? How do we pull in the correct region?
 CREDSTASH="/usr/local/bin/credstash -r ap-southeast-2"
 DB_USERNAME_KEY="${ENV^}"GeodesyDbUsername

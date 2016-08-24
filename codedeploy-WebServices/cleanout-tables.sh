64d8f25fb8865c352bb7a99d38436fbb50e1f6eb
lbodor <lazar.bodor@ga.gov.au>
2016-08-24 15:29:33 +1000

    Cleanout database tables when deploying to dev

--- codedeploy-WebServices/cleanout-tables.sh
+++ codedeploy-WebServices/cleanout-tables.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
 
-DB_ID=$1
+. ${BASH_SOURCE%/*}/env.sh
 
-if [ "${DB_ID}" == "geodesy-dev" ]; then
-	AWS_CMD="aws --region ap-southeast-2"
-	RDS_ENDPOINT=$(${AWS_CMD} rds describe-db-instances --db-instance-identifier ${DB_ID} | grep Address | awk -F'"' {'print $4'})
-
-	psql --host=${RDS_ENDPOINT} --port=5432 --username geodesy   --dbname=geodesy << 'EOF'
-	DO
-	$func$
-	BEGIN
-	  EXECUTE
-	  (SELECT 'TRUNCATE TABLE '
-		   || string_agg( 'geodesy.' || quote_ident(tablename), ', ')
-		   || ' CASCADE'
-	   FROM   pg_tables
-	   WHERE  schemaname = 'geodesy'
-       AND    tablename not in ('databasechangelog', 'databasechangeloglock')
-	   );
-	END
-	$func$;
+if [[ ${ENV,,} == "dev" ]]; then
+    PGPASSWORD=${DB_PASSWORD} psql -qtA --host=${RDS_ENDPOINT} --port=5432 --username ${DB_USERNAME} --dbname=${DB_NAME} << 'EOF'
+DO
+$func$
+BEGIN
+    EXECUTE
+    (SELECT 'TRUNCATE TABLE '
+             || string_agg( 'geodesy.' || quote_ident(tablename), ', ')
+             || ' CASCADE'
+     FROM   pg_tables
+     WHERE  schemaname = 'geodesy'
+        AND    tablename not in ('databasechangelog', 'databasechangeloglock')
+     );
+END
+$func$;
 EOF
-	echo "Cleaned out tables in dev database"
+    echo "Cleaned out tables in dev database"
 else
-	echo "Not dev database, not cleaning tables"
+    echo "Not dev database, not cleaning tables"
 fi
 

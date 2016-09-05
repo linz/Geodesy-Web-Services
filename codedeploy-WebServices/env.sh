4fe26e128a46062d9e755e486f48330895142af8
lbodor <lazar.bodor@ga.gov.au>
2016-09-05 15:04:34 +1000

    Seperate credstash keys from stack parameter names

--- codedeploy-WebServices/env.sh
+++ codedeploy-WebServices/env.sh
@@ -22,8 +22,8 @@ RDS_ENDPOINT=$(${AWS} rds describe-db-instances --db-instance-identifier ${RDS_I
 
 CREDSTASH="/usr/local/bin/credstash -r ${AWS_DEFAULT_REGION}"
 
-RDS_MASTER_USERNAME_KEY=${ENV^}GeodesyDbRdsUsername
-RDS_MASTER_PASSWORD_KEY=${ENV^}GeodesyDbRdsPassword
+RDS_MASTER_USERNAME_KEY=${ENV^}GeodesyDbRdsMasterUsername
+RDS_MASTER_PASSWORD_KEY=${ENV^}GeodesyDbRdsMasterPassword
 RDS_MASTER_USERNAME=$(${CREDSTASH} get ${RDS_MASTER_USERNAME_KEY})
 RDS_MASTER_PASSWORD=$(${CREDSTASH} get ${RDS_MASTER_PASSWORD_KEY})
 

06f346e3d4590fe637e804ba58b01e62e7b01422
Lazar Bodor <lazar.bodor@ga.com>
2016-09-07 15:21:54 +1000

    Send site log upload errors to client
    Former-commit-id: 61200968866d60cfd414160ed512734f51dfb7b9

--- src/test/java/au/gov/ga/geodesy/port/adapter/rest/UploadInvalidSiteLogRestTest.java
+++ src/test/java/au/gov/ga/geodesy/port/adapter/rest/UploadInvalidSiteLogRestTest.java
@@ -3,6 +3,8 @@ package au.gov.ga.geodesy.port.adapter.rest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.springframework.http.MediaType;
 import org.testng.annotations.Test;
 
@@ -10,7 +12,10 @@ public class UploadInvalidSiteLogRestTest extends RestTest {
 
     @Test
     public void uploadInvalidSopacSiteLog() throws Exception {
-        mvc.perform(post("/siteLogs/sopac/upload").contentType(MediaType.APPLICATION_XML).content("Invalid site log content"))
-            .andExpect(status().isBadRequest());
+        String response = mvc.perform(post("/siteLogs/sopac/upload").contentType(MediaType.APPLICATION_XML).content("Invalid site log content"))
+            .andExpect(status().isBadRequest())
+            .andReturn().getResponse().getContentAsString();
+
+        MatcherAssert.assertThat(response, Matchers.not(Matchers.isEmptyOrNullString()));
     }
 }

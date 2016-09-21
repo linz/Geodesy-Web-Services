1af70e218055f20e0c70cfb9564ceaf37813f526
Hong Jin <hong.jin@ga.gov.au>
2016-09-21 15:26:31 +1000

    refact equipment configuration relationship
    Former-commit-id: c89fb508059febaf65552b202b32f788dd426ec9

---
+++ src/main/resources/db/add_records_to_equipment_configuration.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+--changeset Hong Jin:id 1474424364
+
+
+insert into equipment_configuration(equipment_configuration_id,equipment_id,configuration_time)
+select a.id, a.equipment_id, a.configuration_time
+from clock_configuration a, equipment b
+where a.equipment_id = b.id 
+union
+select a.id ,a.equipment_id, a.configuration_time
+from gnss_antenna_configuration a, equipment b
+where a.equipment_id = b.id   
+union
+select a.id, a.equipment_id, a.configuration_time
+from gnss_receiver_configuration a, equipment b
+where a.equipment_id = b.id  
+union
+select a.id, a.equipment_id, a.configuration_time
+from humidity_sensor_configuration a, equipment b
+where a.equipment_id = b.id;
+
+commit;
+

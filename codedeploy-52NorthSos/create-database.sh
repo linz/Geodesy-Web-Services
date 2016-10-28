7d749a6a5c2b09e5ae52869e32382c4b6ca884b6
Aaron Sedgmen <aaron.sedgmen@ga.gov.au>
2016-10-28 13:05:00 +1100

    codedeploy for 52north-sensorweb-sos

--- codedeploy-52NorthSos/create-database.sh
+++ codedeploy-52NorthSos/create-database.sh
@@ -11,34 +11,39 @@ DB_SCHEMA="sos"
 PSQL_AS_MASTER_USER="PGPASSWORD=${RDS_MASTER_PASSWORD} psql --host=${RDS_ENDPOINT} --port=5432 --username ${RDS_MASTER_USERNAME}  --dbname=${DB_NAME}"
 PSQL_AS_DB_USER="PGPASSWORD=${DB_PASSWORD} psql --host=${RDS_ENDPOINT} --port=5432 --username ${DB_USERNAME}  --dbname=${DB_NAME}"
 
+# Only proceed if the sos schema containing tables does not already exist
 EXISTING_SCHEMA=$(eval "${PSQL_AS_MASTER_USER} -qtA -c \"SELECT schema_name FROM information_schema.schemata WHERE schema_name = '${DB_SCHEMA}';\"")
 
 if [[ ${EXISTING_SCHEMA} == ${DB_SCHEMA} ]]; then
-  echo "Exiting, schema ${DB_SCHEMA} already exists."
-  exit 0
+
+  EXISTING_TABLES=$(eval "${PSQL_AS_MASTER_USER} -qtA -c \"SELECT count(*) from information_schema.tables where table_schema = '${DB_SCHEMA}';\"")
+
+  if [[ ${EXISTING_TABLES} -gt 0 ]]; then
+    echo "Exiting, schema ${DB_SCHEMA} with tables already exists."
+    exit 0
+  fi
+
 fi
 
+# Ensure db user exists
+EXISTING_USER=$(eval "${PSQL_AS_MASTER_USER} -qtA -c \"SELECT count(*) FROM pg_catalog.pg_user WHERE usename = '${DB_USERNAME}';\"")
+
+if [ $EXISTING_USER -eq 0 ]; then
+echo "Creating database user"
+PGPASSWORD=${RDS_MASTER_PASSWORD} psql --host=${RDS_ENDPOINT} --port=5432 --username ${RDS_MASTER_USERNAME}  --dbname=${DB_NAME} << EOF
+create user ${DB_USERNAME} with password '${DB_PASSWORD}';
+alter database "${DB_NAME}" owner to ${DB_USERNAME};
+EOF
+fi
+
+# Ensure the schema exists
 PGPASSWORD=${RDS_MASTER_PASSWORD} psql --host=${RDS_ENDPOINT} --port=5432 --username ${RDS_MASTER_USERNAME}  --dbname=${DB_NAME} << EOF
 drop schema if exists ${DB_SCHEMA} CASCADE;
-DO
-$body$
-BEGIN
-   IF NOT EXISTS (
-      SELECT *
-      FROM   pg_catalog.pg_user
-      WHERE  usename = '${DB_USERNAME}') THEN
-
-      CREATE ROLE my_user LOGIN PASSWORD '${DB_PASSWORD}';
-   END IF;
-END
-$body$;
-alter database "${DB_NAME}" owner to ${DB_USERNAME};
 create schema ${DB_SCHEMA} authorization ${DB_USERNAME};
-grant ${DB_SCHEMA} to postgres;
-create extension postgis;
 EOF
 
+# Populate the schema with 52 North SOS database objects
 PGPASSWORD=${DB_PASSWORD} psql --host=${RDS_ENDPOINT} --port=5432 --username ${DB_USERNAME}  --dbname=${DB_NAME} << EOF
 \set schema '${DB_SCHEMA}'
-\i ./PG_series_script_create.sql
+\i ${BASH_SOURCE%/*}/PG_series_script_create.sql
 EOF

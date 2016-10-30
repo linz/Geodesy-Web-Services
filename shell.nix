c9fb22b05e4bf1579bfaea3e8e190adec394a870
lbodor <lazar.bodor@ga.gov.au>
2016-10-31 10:49:09 +1100

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,20 @@
 let
-  pkgs = import <nixpkgs> {};
-  env = with pkgs; buildEnv {
-    name = "geodesyDevEnv";
+  defaultPkgs = import <nixpkgs> {};
+  pinnedPkgs = import (defaultPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "354fd3728952c229fee4f2924737c601d7ab4725";
+    sha256 = "052hfh3w2086zaaskx0y0dz4y6y2bpvj0xqlcarpbwn2ccnpph0j";
+  }) {};
+
+in
+
+{ nixpkgs ? pinnedPkgs }:
+
+let
+  pkgs = if nixpkgs == null then defaultPkgs else pinnedPkgs;
+  devEnv = with pkgs; buildEnv {
+    name = "devEnv";
     paths = [
       awscli
       python
@@ -13,11 +26,6 @@ let
 in
   pkgs.runCommand "dummy" {
     buildInputs = [
-      env
+      devEnv
     ];
   } ""
-
-
-
-
-

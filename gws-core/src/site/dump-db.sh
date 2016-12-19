05a9d613b16e8b22d4d5916c195e20daaa629256
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-12-19 14:52:35 +1100

    Fix database port used for dumping db schema

--- gws-core/src/site/dump-db.sh
+++ gws-core/src/site/dump-db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pg_dumpall -U postgres -h localhost -p 5433 --globals-only --no-tablespaces > target/site/create-db.sql
-pg_dump -U geodesy -h localhost -p 5433 --clean --create geodesydb >> target/site/create-db.sql
+pg_dumpall -U postgres -h localhost -p 5434 --globals-only --no-tablespaces > target/site/create-db.sql
+pg_dump -U geodesy -h localhost -p 5434 --clean --create geodesydb >> target/site/create-db.sql

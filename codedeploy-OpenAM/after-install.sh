a4f5b01bf743f5a7b756166e4d121b142af425c2
Aaron Sedgmen <aaron.sedgmen@ga.gov.au>
2017-01-18 14:11:15 +1100

    Changes to get codeploy working

--- codedeploy-OpenAM/after-install.sh
+++ codedeploy-OpenAM/after-install.sh
@@ -12,11 +12,22 @@ sed -i 's/${OPENAM_DS_DIRMGRPASSWD}/'"${OPENAM_DS_DIRMGRPASSWD}/" $OPENAM_CONFIG
 
 # Wait if load balancer is failing health check (Configuration process requires http access to the
 # OpenAM application via the load balancer)
-until curl -s --head --connect-timeout 2 https://${OPENAM_SERVER_FQDN}/openam/images/gradlogsides.jpg | head -n 1 | grep 200 > /dev/null
-do
-  echo "waiting 10 seconds for load balancer to resume passing requests to openam server"
-  sleep 10
-done
+#COUNTER=0
+#until curl -s --head --connect-timeout 2 http://${OPENAM_SERVER_FQDN}/openam/images/gradlogsides.jpg | head -n 1 | grep 200 > /dev/null
+#do
+#  echo "waiting 10 seconds for load balancer to resume passing requests to openam server"
+#  sleep 10
+#  let COUNTER=COUNTER+1
+#  if [ "$COUNTER" -eq "6" ]; then
+#    echo "Giving up waiting for load balancer to resume passing requests to openam server"
+#    exit 1
+#  fi
+#done
+
+# Ensure the hosts file resolves the FQDN to 127.0.0.1
+if ! grep -q "$OPENAM_SERVER_FQDN" "/etc/hosts"; then
+   echo 127.0.0.1 ${OPENAM_SERVER_FQDN} >> /etc/hosts
+fi
 
 # Apply the configuration
 java -jar /opt/openam/tools/config/openam-configurator-tool-13.5.0.jar -f /opt/openam/tools/config/config.file

af43f16b4a6eb16c1efa9abe1bde4695ef448ee6
Aaron Sedgmen <aaron.sedgmen@ga.gov.au>
2017-01-24 17:14:42 +1100

    Inclusion of keystore files in export

--- codedeploy-OpenAM/after-install.sh
+++ codedeploy-OpenAM/after-install.sh
@@ -11,11 +11,9 @@ sed -i 's/${OPENAM_DS_DIRMGRPASSWD}/'"${OPENAM_DS_DIRMGRPASSWD}/" $OPENAM_CONFIG
 
 
 # Apply subsititions to scripts
-EXPORT_SERVICE_CONFIG_SCRIPT=/opt/openam/tools/admin/scripts/export-service-config.sh
-EXPORT_DIRECTORY_DATA_SCRIPT=/opt/openam/tools/admin/scripts/export-ldif-directory-data.sh
-sed -i 's/${OPENAM_SERVER_FQDN}/'"${OPENAM_SERVER_FQDN}/" $EXPORT_SERVICE_CONFIG_SCRIPT
-sed -i 's/${OPENAM_SERVER_FQDN}/'"${OPENAM_SERVER_FQDN}/" $EXPORT_DIRECTORY_DATA_SCRIPT
-sed -i 's/${OPENAM_DS_DIRMGRPASSWD}/'"${OPENAM_DS_DIRMGRPASSWD}/" $EXPORT_DIRECTORY_DATA_SCRIPT
+EXPORT_OPENAM_SCRIPT=/opt/openam/tools/admin/scripts/export_openam.sh
+sed -i 's/${OPENAM_SERVER_FQDN}/'"${OPENAM_SERVER_FQDN}/" $EXPORT_OPENAM_SCRIPT
+sed -i 's/${OPENAM_DS_DIRMGRPASSWD}/'"${OPENAM_DS_DIRMGRPASSWD}/" $EXPORT_OPENAM_SCRIPT
 
 
 # Wait if load balancer is failing health check (Configuration process requires http access to the
@@ -44,8 +42,7 @@ fi
 # Apply the initial configuration
 java -jar /opt/openam/tools/config/openam-configurator-tool-13.5.0.jar -f /opt/openam/tools/config/config.file
 
-
-# Install the OpenAM command line tools (OpenAM must be running and configured)
+# Install the OpenAM command line tools (OpenAM must be running and have initial configuration)
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 cd /opt/openam/tools/admin
 ./setup -p /usr/share/tomcat8/openam -d /opt/openam/tools/admin/debug -l /opt/openam/tools/admin/log --acceptLicense
@@ -53,20 +50,37 @@ echo ${OPENAM_ADMIN_PWD} > passwdfile
 chmod 400 passwdfile
 
 
-# Import OpenAM service config if an export file exists
-FILE_NAME=${OPENAM_SERVER_FQDN}_servicecfg.xml
-S3_DIR=s3://geodesy-openam/service-config-export
-BASE_DIR=/opt/openam/tools/admin
-IMPORT_DIR=/tmp
+# Import OpenAM configuration and data
+S3_DIR=s3://geodesy-openam/exports/${OPENAM_SERVER_FQDN}
+SERVICE_CFG_FILE=service_cfg.xml
+DIRECTORY_DATA_FILE=embedded_directory_data.ldif
+KEYS_DIRECTORY=keys
+IMPORT_DIR=/tmp/openam_imports
+OPENAM_BASE_DIR=/usr/share/tomcat8/openam
+ADMIN_TOOLS_BASE_DIR=/opt/openam/tools/admin
 
-FILE_COUNT=$(aws s3 ls $S3_DIR/ | grep $FILE_NAME | wc -l)
+# Only proceed with import if all files to be imported exist in the S3 bucket (these are a matched set and cannot be
+# installed seperately)
+SERVICE_CFG_FILE_COUNT=$(aws s3 ls $S3_DIR/ | grep $SERVICE_CFG_FILE | wc -l)
+DIRECTORY_DATA_FILE_COUNT=$(aws s3 ls $S3_DIR/ | grep $DIRECTORY_DATA_FILE | wc -l)
+KEYS_DIRECTORY_COUNT=$(aws s3 ls $S3_DIR/ | grep $KEYS_DIRECTORY | wc -l)
 
-if [ $FILE_COUNT -eq 1 ]; then
+if [ $SERVICE_CFG_FILE_COUNT -eq 1 ] && [ $DIRECTORY_DATA_FILE_COUNT -eq 1 ] && [ $KEYS_DIRECTORY_COUNT -eq 1 ]; then
+  # Prepare local filesystem for downloading export files
+  rm -rf $IMPORT_DIR
+  mkdir -p $IMPORT_DIR/$KEYS_DIRECTORY
+
   # Copy file from S3 bucket to local file system
-  aws s3 cp $S3_DIR/$FILE_NAME /tmp/$FILE_NAME
+  aws s3 cp $S3_DIR/$SERVICE_CFG_FILE $IMPORT_DIR/$SERVICE_CFG_FILE
+  aws s3 cp $S3_DIR/$DIRECTORY_DATA_FILE $IMPORT_DIR/$DIRECTORY_DATA_FILE
+  aws s3 cp $S3_DIR/$KEYS_DIRECTORY $IMPORT_DIR/$KEYS_DIRECTORY/. --recursive
 
-  # Import file into OpenAM
-  echo "y" | $BASE_DIR/openam/bin/ssoadm import-svc-cfg -e geodenc -u amadmin -f $BASE_DIR/passwdfile -X $IMPORT_DIR/$FILE_NAME
+  # Replace keystore files
+  cp -f $IMPORT_DIR/$KEYS_DIRECTORY/* $OPENAM_BASE_DIR/openam/.
+  cp -f $IMPORT_DIR/$KEYS_DIRECTORY/.* $OPENAM_BASE_DIR/openam/.
+
+  # Import service config file into OpenAM
+  echo "y" | $ADMIN_TOOLS_BASE_DIR/openam/bin/ssoadm import-svc-cfg -e geodenc -u amadmin -f $ADMIN_TOOLS_BASE_DIR/passwdfile -X $IMPORT_DIR/$SERVICE_CFG_FILE
 
   # Restart required, wait until Tomcat startup has finished
   service tomcat8 restart
@@ -74,26 +88,10 @@ if [ $FILE_COUNT -eq 1 ]; then
   do
     sleep 10
   done
-else
-  echo "No OpenAM service config File found with name ${FILE_NAME}, nothing to import"
-fi
 
-
-# Import LDAP directory data if an export file exists
-FILE_NAME=${OPENAM_SERVER_FQDN}_embedded_directory.ldif
-S3_DIR=s3://geodesy-openam/directory-data-export
-BASE_DIR=/usr/share/tomcat8/openam/opends/bin
-IMPORT_DIR=/tmp
-
-FILE_COUNT=$(aws s3 ls $S3_DIR/ | grep $FILE_NAME | wc -l)
-
-if [ $FILE_COUNT -eq 1 ]; then
-  # Copy file from S3 bucket to local file system
-  aws s3 cp $S3_DIR/$FILE_NAME /tmp/$FILE_NAME
-
-  # Import data file into LDAP directory
-  $BASE_DIR/import-ldif\
-     --ldifFile /tmp/$FILE_NAME\
+  # Import directory data file into LDAP directory
+  $OPENAM_BASE_DIR/opends/bin/import-ldif\
+     --ldifFile /tmp/$DIRECTORY_DATA_FILE\
      --backendID userRoot\
      --hostName ${OPENAM_SERVER_FQDN}\
      --port 4444\
@@ -101,32 +99,21 @@ if [ $FILE_COUNT -eq 1 ]; then
      --bindPassword ${OPENAM_DS_DIRMGRPASSWD}\
      --trustAll\
      --noPropertiesFile
+
 else
-  echo "No LDIF file found with name ${FILE_NAME}, nothing to import for the LDAP directory"
+  echo "Some or all import files could not be found for FQDN ${OPENAM_SERVER_FQDN}, nothing imported"
 fi
 
 
-# Ensure a cronjob for exporting the OpenAM service config exists
+# Ensure a cronjob for exporting the OpenAM backup files exists
 CRONTAB=$(crontab -l)
 CRONJOB="$(cat <<EOF
 #  Export the OpenAM service config
-0 1 * * * /opt/openam/tools/admin/scripts/export-service-config.sh
+0 1 * * * /opt/openam/tools/admin/scripts/export_openam.sh
 EOF
 )"
 
-if [[ ! $CRONTAB == *"export-service-config"* ]]; then
+if [[ ! $CRONTAB == *"export_openam"* ]]; then
   (echo "${CRONTAB}"$'\n'"${CRONJOB}") | crontab -
 fi
 
-# Ensure a cronjob for exporting the LDAP directory data exists
-CRONTAB=$(crontab -l)
-CRONJOB="$(cat <<EOF
-
-#  Export the LDAP directory data
-1 1 * * * /opt/openam/tools/admin/scripts/export-ldif-directory-data.sh
-EOF
-)"
-
-if [[ ! $CRONTAB == *"export-ldif-directory-data"* ]]; then
-  (echo "${CRONTAB}"$'\n'"${CRONJOB}") | crontab -
-fi

50f03c6f4bd5300979483cc3b6eb92f02c0a79d6
lbodor <lazar.bodor@ga.gov.au>
2017-01-26 12:49:24 +1100

    Add gws-tomcat/env.sh

--- gws-tomcat/entrypoint.sh
+++ gws-tomcat/entrypoint.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-java_link="$(which java)"
-java=$(readlink -f "${java_link}")
-java_bin=$(dirname "${java}")
-JAVA_HOME=$(dirname "${java_bin}")
-export JAVA_HOME
-
-catalina_link="$(which catalina.sh)"
-catalina=$(readlink -f "${catalina_link}")
-tomcat_bin=$(dirname "${catalina}")
-TOMCAT_HOME=$(dirname "${tomcat_bin}")
-export TOMCAT_HOME
+. ./env.sh
 
 mkdir -p "${TOMCAT_HOME}"/webapps/
 if [ -d /webapps ]; then

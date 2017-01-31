d285f335bf9f87602fd213c6ad3ec7ec57f87a39
lbodor <lazar.bodor@ga.gov.au>
2017-01-31 18:14:15 +1100

    Configure OpenAM with test users and groups

--- gws-open-am/configure-openam.sh
+++ gws-open-am/configure-openam.sh
@@ -3,7 +3,9 @@
 set -e
 
 . /env.sh
+. /env-openam.sh
 
+# Deploy OpenAM
 mkdir -p "${TOMCAT_HOME}"/webapps/
 if [ -d /webapps ]; then
     rsync --ignore-missing-args -ur /webapps/ "${TOMCAT_HOME}"/webapps/
@@ -11,5 +13,31 @@ fi
 
 startup.sh
 wait_for_tomcat
-java -jar /opt/openam/tools/config/openam-configurator-tool-13.5.0.jar -f /tmp/openam/openam.cfg
+
+# Perform initial configiration
+java -jar $ADMIN_TOOLS_DIR/config/openam-configurator-tool-13.5.0.jar -f $OPENAM_CFG_FILE
+
+(cd $ADMIN_TOOLS_DIR/admin && ./setup -p $OPENAM_BASE_DIR -d $ADMIN_TOOLS_DIR/admin/debug -l $ADMIN_TOOLS_DIR/admin/log --acceptLicense)
+
+# Import service config file into OpenAM
+echo "$ADMIN_PWD" > $ADMIN_TOOLS_DIR/admin/passwdfile
+echo "y" | $SSOADM import-svc-cfg -e geodenc -u amadmin -f $ADMIN_TOOLS_DIR/admin/passwdfile -X $SERVICE_CFG_FILE
+
+# Replace keystore files
+cp -f $KEYS_DIR/keystore.jceks $OPENAM_BASE_DIR/openam/.
+cp -f $KEYS_DIR/keystore.jks $OPENAM_BASE_DIR/openam/.
+cp -f $KEYS_DIR/.keypass $OPENAM_BASE_DIR/openam/.
+cp -f $KEYS_DIR/.storepass $OPENAM_BASE_DIR/openam/.
+
+# Import directory data file into LDAP directory
+$IMPORT_LDIF\
+ --ldifFile $DIRECTORY_DATA_FILE\
+ --backendID userRoot\
+ --hostName localhost\
+ --port 4444\
+ --bindDN cn=Directory\ Manager\
+ --bindPassword $DS_DIRMGRPASSWD\
+ --trustAll\
+ --noPropertiesFile
+
 shutdown.sh

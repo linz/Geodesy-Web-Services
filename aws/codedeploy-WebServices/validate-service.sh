5ba62b15e3b768305292fcf08f1e9a2bb426cf81
lbodor <lazar.bodor@ga.gov.au>
2017-05-04 15:42:40 +1000

    Run GWS system tests to validate a deployment

--- aws/codedeploy-WebServices/validate-service.sh
+++ aws/codedeploy-WebServices/validate-service.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-echo service validated
+
+set -e
+
+. ${BASH_SOURCE%/*}/env.sh
+
+cd ${BASH_SOURCE%/*}
+
+# Run system tests
+java -jar gws-system-test.jar \
+    -testjar gws-system-test.jar \
+    -DwebServicesUrl=http://localhost:8080
+    -DoauthProviderUrl=https://${ENV}geodesy-openam.geodesy.ga.gov.au/openam/oauth2

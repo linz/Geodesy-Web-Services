2b43b5792f3aaa1c31e3aeef3bb9b7fec3e409fe
lbodor <lazar.bodor@ga.gov.au>
2017-05-30 19:42:47 +1000

    Lookup SNS topics by stack, as well as event name

---
+++ gws-system-test/src/main/java/au/gov/ga/geodesy/gws/systemtest/AwsSystemTest.java
@@ -0,0 +1,35 @@
+package au.gov.ga.geodesy.gws.systemtest;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Test;
+
+import au.gov.ga.geodesy.support.aws.Aws;
+
+import io.restassured.response.ValidatableResponse;
+
+public class AwsSystemTest extends BaseSystemTest {
+
+    @Test
+    public void testGetStackName() {
+        ValidatableResponse response = given()
+            .when()
+            .get(getConfig().getWebServicesUrl() + "/aws/stackName")
+            .then();
+
+        if (Aws.inAmazon()) {
+            String stackName = response
+                .statusCode(HttpStatus.OK.value())
+                .extract().body().asString();
+
+            assertThat(stackName, is("DevGeodesy"));
+        } else {
+            response.statusCode(HttpStatus.NO_CONTENT.value());
+        }
+    }
+}
+

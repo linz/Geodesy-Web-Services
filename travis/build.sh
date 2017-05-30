e327b0f20b6468cf64b469f81c1b6d6fa4925b4b
lbodor <lazar.bodor@ga.gov.au>
2017-05-31 09:23:25 +1000

    Deploy branch `master` to test

--- travis/build.sh
+++ travis/build.sh
@@ -8,14 +8,21 @@ sudo rm -f /etc/mavenrc
 
 mvn -U --settings ./travis/maven-settings.xml compile
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "next" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     mvn --settings ./travis/maven-settings.xml deploy -pl '!gws-system-test'
     mvn --settings ./travis/maven-settings.xml site-deploy -DskipTests -pl gws-core
     aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}" --profile geodesy
     aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}" --profile geodesy
     aws configure set region ap-southeast-2 --profile geodesy
     aws configure set output json --profile geodesy
-    ./aws/codedeploy-WebServices/deploy.sh dev
+    case "${TRAVIS_BRANCH}" in
+        "next")
+            ./aws/codedeploy-WebServices/deploy.sh dev
+        ;;
+        "master")
+            ./aws/codedeploy-WebServices/deploy.sh test
+        ;;
+    esac
 else
     mvn --settings ./travis/maven-settings.xml verify -pl '!gws-system-test'
 fi

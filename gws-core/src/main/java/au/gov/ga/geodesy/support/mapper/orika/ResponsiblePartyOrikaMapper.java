7541f282d12fdfaf71a9dc95b9ea47c9e4456ea7
lbodor <lazar.bodor@ga.gov.au>
2017-06-21 15:11:26 +1000

    Map responsible party online resource element

--- gws-core/src/main/java/au/gov/ga/geodesy/support/mapper/orika/ResponsiblePartyOrikaMapper.java
+++ gws-core/src/main/java/au/gov/ga/geodesy/support/mapper/orika/ResponsiblePartyOrikaMapper.java
@@ -1,7 +1,13 @@
 package au.gov.ga.geodesy.support.mapper.orika;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import org.geotools.metadata.iso.citation.AddressImpl;
 import org.geotools.metadata.iso.citation.ContactImpl;
+import org.geotools.metadata.iso.citation.OnLineResourceImpl;
 import org.geotools.metadata.iso.citation.ResponsiblePartyImpl;
 import org.geotools.metadata.iso.citation.TelephoneImpl;
 import org.geotools.util.SimpleInternationalString;
@@ -10,10 +16,14 @@ import org.opengis.util.InternationalString;
 
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.metadata.Type;
 
 import net.opengis.iso19139.gmd.v_20070417.CIAddressType;
 import net.opengis.iso19139.gmd.v_20070417.CIContactType;
+import net.opengis.iso19139.gmd.v_20070417.CIOnlineResourceType;
 import net.opengis.iso19139.gmd.v_20070417.CIResponsiblePartyType;
 import net.opengis.iso19139.gmd.v_20070417.CITelephoneType;
 
@@ -40,7 +50,13 @@ public class ResponsiblePartyOrikaMapper {
         mapperFactory.classMap(ContactImpl.class, CIContactType.class)
             .field("address", "address.CIAddress")
             .field("phone", "phone.CITelephone")
+            .field("onLineResource", "onlineResource.CIOnlineResource")
             .byDefault()
+            .register();
+
+        mapperFactory.classMap(OnLineResourceImpl.class, CIOnlineResourceType.class)
+            .field("linkage", "linkage.URL")
+            .field("protocol", "protocol")
             .register();
 
         mapperFactory.classMap(TelephoneImpl.class, CITelephoneType.class)
@@ -49,10 +65,13 @@ public class ResponsiblePartyOrikaMapper {
             .byDefault()
             .register();
 
+        mapperFactory.registerConcreteType(InternationalString.class, SimpleInternationalString.class);
+
         mapperFactory.getConverterFactory().registerConverter(new InternationalStringToStringPropertyConverter());
         mapperFactory.getConverterFactory().registerConverter(new RoleToCodeListValueConverter());
         mapperFactory.getConverterFactory().registerConverter(new StringToStringPropertyConverter());
-        mapperFactory.registerConcreteType(InternationalString.class, SimpleInternationalString.class);
+        mapperFactory.getConverterFactory().registerConverter(new UriToStringConverter());
+
         mapper = mapperFactory.getMapperFacade();
     }
 

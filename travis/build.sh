52d540a91ec4c849fbcf255c4fdef825bee8c6e8
lbodor <lazar.bodor@ga.gov.au>
2017-07-25 11:24:22 +1000

    Get latest snapshot dependencies

--- travis/build.sh
+++ travis/build.sh
@@ -8,6 +8,9 @@ sudo rm -f /etc/mavenrc
 
 # We redirect maven test output to file, because Travis CI limits stdout log size to 4MB.
 
+# Get latest snapshot dependencies
+mvn dependency:resolve -U
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     mvn --settings ./travis/maven-settings.xml deploy -pl '!gws-system-test' -DredirectTestOutputToFile
     mvn --settings ./travis/maven-settings.xml deploy -pl gws-system-test -DskipTests

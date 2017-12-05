fbd7b742e5fe7a1fe8d437a0b20b1924e42d89df
lbodor <lazar.bodor@ga.gov.au>
2017-12-06 07:55:45 +1100

    Run site-deploy only on master branch

--- travis/build.sh
+++ travis/build.sh
@@ -13,7 +13,7 @@ mvn --settings ./travis/maven-settings.xml -U install -DskipTests > /dev/null
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     mvn --settings ./travis/maven-settings.xml deploy -pl '!gws-system-test' -DredirectTestOutputToFile
     mvn --settings ./travis/maven-settings.xml deploy -pl gws-system-test -DskipTests
-    mvn --settings ./travis/maven-settings.xml site-deploy -DskipTests -pl gws-core
+    mvn --settings ./travis/maven-settings.xml site -DskipTests -pl gws-core
     cp ./gws-webapp/target/geodesy-web-services.war ./gws-system-test/target/gws-system-test.jar ./aws/codedeploy-WebServices/
     aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}" --profile geodesy
     aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}" --profile geodesy
@@ -29,6 +29,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
             ./aws/codedeploy-WebServices/deploy.sh test
         ;;
         "master")
+            mvn --settings ./travis/maven-settings.xml site-deploy -DskipTests -pl gws-core
             ./aws/codedeploy-WebServices/deploy.sh test
         ;;
     esac

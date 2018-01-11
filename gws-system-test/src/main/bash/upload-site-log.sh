482624e84827c686c528f305bafc79896131886d
Jeffery Liang <jliang@linz.govt.nz>
2018-01-12 08:57:40 +1300

    deployments update for LINZ

--- gws-system-test/src/main/bash/upload-site-log.sh
+++ gws-system-test/src/main/bash/upload-site-log.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-gws=http://localhost:8081
-openam=http://localhost:8083/openam
+gws=https://devgeodesy-webservices.fsitelog.ga/
+openam=https://devgeodesy-openam.fsitelog.ga/openam
 
 clientId=GnssSiteManager
 clientPassword=
 
-username=
-password=
+username=user.x
+password=gumby123X
 
 jwt=$(curl -s --user ${clientId}:${clientPassword} --data "grant_type=password&username=${username}&password=${password}&scope=openid profile" ${openam}/oauth2/access_token?realm=/ | jq .id_token | tr -d '"')
 
-curl --data-binary @${1} ${gws}/siteLogs/upload \
+curl -v --data-binary @${1} ${gws}/siteLogs/upload \
     -H "Authorization: Bearer ${jwt}"
